b07fc3e39ff5404d0ea6b4eb5c07184747909f05
Håkan Johansson <aq.hakan@gmail.com>
2026-06-04 16:58:26 +0200

    Removed reference to medians.

--- R/load_ref_data.R
+++ R/load_ref_data.R
@@ -10,12 +10,26 @@ load_ref_data <- function() {
     stop(sprintf("Reference file not found: %s", filename))
   }
 
-  population <- readxl::read_excel(
+  raw_population <- readxl::read_excel(
     ref_path,
-    col_names = c(
-      "group", "mean", "sd", "median", "2_3_range", "q_1_6", "q_5_6", "n"
-    ),
+    col_names = FALSE,
     skip = 1
   )
+
+  if (ncol(raw_population) < 8) {
+    stop("Reference file has an unexpected format.")
+  }
+
+  population <- data.frame(
+    group = raw_population[[1]],
+    mean = raw_population[[2]],
+    sd = raw_population[[3]],
+    `2_3_range` = raw_population[[5]],
+    q_1_6 = raw_population[[6]],
+    q_5_6 = raw_population[[7]],
+    n = raw_population[[8]],
+    stringsAsFactors = FALSE
+  )
+
   return(population)
 }
